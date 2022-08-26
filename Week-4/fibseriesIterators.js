23544a9d2132aad28213256d92b3af9283fff6c1
sai1502 <48243234+sai1502@users.noreply.github.com>
2022-08-26 14:26:08 +0530

    Add files via upload

---
+++ Week-4/fibseriesIterators.js
@@ -0,0 +1,21 @@
+let Fib = (n) => ({
+    [Symbol.iterator]: () => {
+        let i = 1;
+        let old = 0;
+        let new1 = 0;
+        return {
+            next: () => {
+                if(i++ <= n){
+                    [old, new1] = [new1, (old + new1) || 1];
+                    return {value: old, done: false}
+                }else{
+                    return {done : true}
+                }
+            }
+        }
+    }
+});
+
+for(let num of Fib(6)){
+    console.log(num);
+}
